7ae1f401b30458e77d0e8a95b28e1bb304161a2c
hansamk97 <134520462+hansamk97@users.noreply.github.com>
2023-10-10 08:58:29 +0900

    Add files via upload

---
+++ day09/jpa/src/main/java/com/codingbox/jpa/JpaMain.java
@@ -0,0 +1,53 @@
+package com.codingbox.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf 
+			= Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		Member member = new Member();
+		
+		// 추가
+		member.setId(7L);
+		member.setName("UserC");
+		
+		// 회원 조회
+		Member findMember = em.find(Member.class, 3L);
+		System.out.println("findMember.id : " + findMember.getId());
+		System.out.println("findMember.name : " + findMember.getName());
+		
+		// 회원 수정
+		findMember.setName("SuperUser");
+		
+		// 회원 삭제
+		em.remove(findMember);
+		
+		
+		em.persist(member);	
+		
+		tx.commit();
+		em.close();
+		emf.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ day09/jpa/src/main/java/com/codingbox/jpa/JpaMain03.java
@@ -0,0 +1,46 @@
+package com.codingbox.jpa;
+
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain03 {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf 
+			= Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		// 비영속 상태
+		Member member = new Member();
+		member.setId(100L);
+		member.setName("100user");
+		
+		// 여기서부터가 영속상태
+		// EntityManager 안에있는 영속 컨텍스트에 관리가 된다는 뜻
+		System.out.println("----- befor ------");
+		em.persist(member);		// insert???
+		System.out.println("----- after ------");
+		
+		tx.commit();
+		em.close();
+		emf.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
